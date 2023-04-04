f22bf8c9d32a4cec5f4a999da8350a3c2ac5e19c
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-04-04 18:05:47 +0100

    method added but spassed arg showing as undefined on second loop meaning only the first letter of the passed arg is printed

--- app/javascript/custom/home.js
+++ app/javascript/custom/home.js
@@ -1,1 +1,18 @@
-console.log("loaded");
+const headingUpper = "Hello, I'm Chris";
+const headinglower = "I build things for the web";
+const speed = 50; // speed milleseconds
+let count = 0;
+let printedText;
+
+typeEffect(headingUpper, "headingUpper");
+
+function typeEffect(printedText, target) {
+    if (count < printedText.length) {
+        document.getElementById(target).innerHTML += printedText.charAt(count);
+        count++;
+        setTimeout(typeEffect, speed);
+        // console.log(typeof printedText);
+    }
+}
+
+console.log("connected")
