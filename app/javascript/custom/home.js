4653fa85eaa5a559f59fca94e5fb79ce80199c41
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-04-05 14:32:42 +0100

    smooth transitions to page section on navbar click implented using method within home.js

--- app/javascript/custom/home.js
+++ app/javascript/custom/home.js
@@ -24,3 +24,18 @@ async function runTypeEffect() {
 }
 
 runTypeEffect();
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+for (let item of anchorlinks) {
+    item.addEventListener('click', (e) => {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
