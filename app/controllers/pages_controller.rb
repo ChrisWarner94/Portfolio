d2ca9c04068a0d37292ef13108b551f588b111b1
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-04-11 00:22:31 +0100

    git hub api calls partially working switching back to main so that I can address issues accross the website ahead of hosting on domain tomorrow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,33 +1,36 @@
+
+
 class PagesController < ApplicationController
   def home
     @technologies = Technology.all
     
-    # projects_array = []
+    projects_array = []
 
-    # # pull public repos from github 
+    # pull public repos from github 
     
-    # call_url = 'https://api.github.com/users/ChrisWarner94/repos'
-    # repos_serialized = URI.open(call_url).read
-    # repos = JSON.parse(repos_serialized)
-    # @repos = repos 
+    call_url = "https://api.github.com/users/ChrisWarner94/repos"
+    repos_serialized = URI.open(call_url).read
+    repos = JSON.parse(repos_serialized)
+    @repos = repos 
 
-    # repos.each do |repo|
+    repos.each do |repo|
 
-    #     repository = Project.new(
-    #         name = repo[:name]
-    #         #url = repo[:html_url]
-    #         #description = [:description]
-    #     )
-    #     projects_array << repository
-    # end
+        repository = Project.new(
+            name: repo["name"],
+            url: repo["html_url"],
+            description: [:description]
+        )
+        projects_array << repository
+        @display_projects = projects_array.last(3)
+    end
 
-    # @projects = projects_array
+    @projects = projects_array
 
-    # # pull languages included in each repo
-    # call_url = "https://api.github.com/repos/ChrisWarner94/Recipeasy/languages" # eachdo |key, value| key for language name watch out for shell
-    # language_serialized = URI.open(call_url).read
-    # languages = JSON.parse(language_serialized)
-    # @languages = languages 
+    # pull languages included in each repo
+    call_url = "https://api.github.com/repos/ChrisWarner94/Recipeasy/languages" # eachdo |key, value| key for language name watch out for shell
+    language_serialized = URI.open(call_url).read
+    languages = JSON.parse(language_serialized)
+    @languages = languages 
 
     # call_url = 'https://api.github.com/repos/ChrisWarner94/Recipeasy/commits' # date for time and date
     # commits_serialized = URI.open(call_url).read
--- app/javascript/custom/home.js
+++ app/javascript/custom/home.js
@@ -1,5 +1,5 @@
-const headingOne = "Hello, I'm Chris."
-const headingTwo = "I build things for the web."
+const headingOne = "Christopher Warner"
+const headingTwo = "Full-Stack Developer."
 const intro = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 function typeEffect(element, text, speed) {
