d44d343fd19589147e04fed8f5c28efc144ab22f
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-04-10 21:48:19 +0100

    env file added and insturction passed to git ignore - need to authenticate to bypass API call limit set in place for github

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,36 @@
 class PagesController < ApplicationController
   def home
     
+    
+    projects_array = []
+
     # pull public repos from github 
     @technologies = Technology.all
-    url = 'https://api.github.com/users/ChrisWarner94/repos'
-    repos_serialized = URI.open(url).read
+    call_url = 'https://api.github.com/users/ChrisWarner94/repos'
+    repos_serialized = URI.open(call_url).read
     repos = JSON.parse(repos_serialized)
     @repos = repos 
 
+    repos.each do |repo|
+        
+        repository = Project.new(
+            name = repo[:name]
+            #url = repo[:html_url]
+            #description = [:description]
+        )
+        projects_array << repository
+    end
+
+    @projects = projects_array
+
     # pull languages included in each repo
-    url = "https://api.github.com/repos/ChrisWarner94/Recipeasy/languages" # eachdo |key, value| key for language name watch out for shell
-    language_serialized = URI.open(url).read
+    call_url = "https://api.github.com/repos/ChrisWarner94/Recipeasy/languages" # eachdo |key, value| key for language name watch out for shell
+    language_serialized = URI.open(call_url).read
     languages = JSON.parse(language_serialized)
     @languages = languages 
 
-    url = 'https://api.github.com/repos/ChrisWarner94/Recipeasy/commits' # date for time and date
-    commits_serialized = URI.open(url).read
+    call_url = 'https://api.github.com/repos/ChrisWarner94/Recipeasy/commits' # date for time and date
+    commits_serialized = URI.open(call_url).read
     commits = JSON.parse(commits_serialized)
     @commits = commits 
   end
