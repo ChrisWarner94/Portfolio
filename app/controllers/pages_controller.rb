44cfe6b6e4b8499aa79b1a0108955f5e18605865
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-04-10 22:36:50 +0100

    auth

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,38 +1,38 @@
 class PagesController < ApplicationController
   def home
-    
-    
-    projects_array = []
-
-    # pull public repos from github 
     @technologies = Technology.all
-    call_url = 'https://api.github.com/users/ChrisWarner94/repos'
-    repos_serialized = URI.open(call_url).read
-    repos = JSON.parse(repos_serialized)
-    @repos = repos 
+    
+    # projects_array = []
 
-    repos.each do |repo|
-        
-        repository = Project.new(
-            name = repo[:name]
-            #url = repo[:html_url]
-            #description = [:description]
-        )
-        projects_array << repository
-    end
+    # # pull public repos from github 
+    
+    # call_url = 'https://api.github.com/users/ChrisWarner94/repos'
+    # repos_serialized = URI.open(call_url).read
+    # repos = JSON.parse(repos_serialized)
+    # @repos = repos 
 
-    @projects = projects_array
+    # repos.each do |repo|
 
-    # pull languages included in each repo
-    call_url = "https://api.github.com/repos/ChrisWarner94/Recipeasy/languages" # eachdo |key, value| key for language name watch out for shell
-    language_serialized = URI.open(call_url).read
-    languages = JSON.parse(language_serialized)
-    @languages = languages 
+    #     repository = Project.new(
+    #         name = repo[:name]
+    #         #url = repo[:html_url]
+    #         #description = [:description]
+    #     )
+    #     projects_array << repository
+    # end
 
-    call_url = 'https://api.github.com/repos/ChrisWarner94/Recipeasy/commits' # date for time and date
-    commits_serialized = URI.open(call_url).read
-    commits = JSON.parse(commits_serialized)
-    @commits = commits 
+    # @projects = projects_array
+
+    # # pull languages included in each repo
+    # call_url = "https://api.github.com/repos/ChrisWarner94/Recipeasy/languages" # eachdo |key, value| key for language name watch out for shell
+    # language_serialized = URI.open(call_url).read
+    # languages = JSON.parse(language_serialized)
+    # @languages = languages 
+
+    # call_url = 'https://api.github.com/repos/ChrisWarner94/Recipeasy/commits' # date for time and date
+    # commits_serialized = URI.open(call_url).read
+    # commits = JSON.parse(commits_serialized)
+    # @commits = commits 
   end
 end
 
