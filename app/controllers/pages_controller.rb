14c792a9e0efb6051b7e7de6cc4cfc491bb19a33
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-04-11 15:17:49 +0100

    projects model removed to be replaced with commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,11 @@
-
+require 'open-uri'
 
 class PagesController < ApplicationController
   def home
+    call_url = 'https://api.github.com/repos/ChrisWarner94/Recipeasy/commits' # date for time and date
+    commits_serialized = URI.open(call_url).read
+    commits = JSON.parse(commits_serialized)
     
-    
-
-    # call_url = 'https://api.github.com/repos/ChrisWarner94/Recipeasy/commits' # date for time and date
-    # commits_serialized = URI.open(call_url).read
-    # commits = JSON.parse(commits_serialized)
-    # @commits = commits 
   end
 end
 
