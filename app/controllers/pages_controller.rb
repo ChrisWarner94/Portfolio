d2a3a299b6ca694bd919a522b6a7f71cf38a60c9
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-04-11 16:24:34 +0100

    git hub api call working date-time needs formatting correctly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,35 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   def home
-    call_url = 'https://api.github.com/repos/ChrisWarner94/Recipeasy/commits' # date for time and date
-    commits_serialized = URI.open(call_url).read
-    commits = JSON.parse(commits_serialized)
     
+    # arrays need for hangling github api data and cleansing
+    @commits_array = []
+    @commit_details = []
+    details_array = []
+
+    # API call
+    call_url = 'https://api.github.com/repos/ChrisWarner94/Recipeasy/commits'
+    commits_serialized = URI.open(call_url).read
+    @commits = JSON.parse(commits_serialized)
+    
+    # loops through each instance in @commits and cleans the data and passes it as an array to @commit details array
+    @commits.each do |commit|
+      commit_date_time = format_date(commit["commit"]["author"]["date"])
+      commit_owner = commit["commit"]["author"]["name"]
+      commit_message = commit["commit"]["message"]
+
+      details_array << commit_date_time
+      details_array << commit_owner
+      details_array << commit_message
+      @commit_details << details_array
+
+    end
+  end
+
+  private # ------------------------------------------ private methods ------------------------------------------ #
+
+  def format_date(date_unformatted)
+
   end
 end
 
