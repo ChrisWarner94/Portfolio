4d3936b44345c24cbf3675ce56ec70fd804a10df
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-04-13 18:47:01 +0100

    divs now arranged properly in home html - js connected up properly to html tags -

--- app/javascript/custom/home.js
+++ app/javascript/custom/home.js
@@ -19,8 +19,10 @@ function typeEffect(element, text, speed) {
     });
 }
 
+runTypeEffect();
+
 function fadeIn(element, speed) {
-    introText.innerHTML = "Hello there! I'm thrilled to welcome you to my profile site. I'm a Full Stack Web Developer who has recently completed an intensive coding program, where I gained expertise in both front - end and back - end development.I'm passionate about creating visually appealing and user-friendly web applications that provide meaningful experiences to users. With a keen eye for design and a knack for problem-solving, I enjoy bringing ideas to life using the latest web technologies and techniques. Take a moment to explore my profile site and learn more about my skills."
+    element.innerHTML = "Hello there! I'm thrilled to welcome you to my profile site. I'm a Full Stack Web Developer who has recently completed an intensive coding program, where I gained expertise in both front - end and back - end development.I'm passionate about creating visually appealing and user-friendly web applications that provide meaningful experiences to users. With a keen eye for design and a knack for problem-solving, I enjoy bringing ideas to life using the latest web technologies and techniques. Take a moment to explore my profile site and learn more about my skills."
     return new Promise((resolve) => {
         let opacity = 0;
         let top = 20;
@@ -42,9 +44,9 @@ function fadeIn(element, speed) {
 
 
 async function runTypeEffect() {
-    await typeEffect(headingUpper, headingOne, 35);
-    await typeEffect(headingLower, headingTwo, 35);
-    await fadeIn(introText, 6);
+    await typeEffect(nameheading, headingOne, 35);
+    await typeEffect(jobtitleheading, headingTwo, 35);
+    await fadeIn(introtext, 6);
 }
 
 // method for navbar hiding and displaying based on user scroll
@@ -52,14 +54,14 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar-container").style.top = "0";
+        document.getElementById("navbarcontainer").style.top = "0";
     } else {
-        document.getElementById("navbar-container").style.top = "-10%";
+        document.getElementById("navbarcontainer").style.top = "-10%";
     }
     prevScrollpos = currentScrollPos;
 }
 
-runTypeEffect();
+
 
 
 // method for the smooth scroll to anchor on navlink click
