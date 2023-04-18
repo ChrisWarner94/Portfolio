5b97a80a398438203877b9af1d7b91d292384b85
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-04-18 14:03:23 +0100

    project model added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   def home
     
     @technologies = ["Ruby", "Rails", "C#", "Javascript", "HTML", "CSS", "PostgreSQL", "SQLite", "Git", "SCSS", "Bootstrap", "MVC"]
+    @projects = ["Recipeasy", "Enna", "Magic Motors"]
 
     # arrays need for hangling github api data and cleansing
     
---
+++ db/migrate/20230418130305_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :picture_url
+      t.string :short_description
+      t.text :long_description
+      t.text :demo_url
+      t.text :host_link
+      t.text :github_link
+
+      t.timestamps
+    end
+  end
+end
