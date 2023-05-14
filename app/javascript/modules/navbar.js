880aa6ca60f83510dbf6d49fb23978f6540e4d45
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-05-15 00:00:00 +0100

    list style none added to apllication scss file - navbar now responding to click with complete scss rhtml rework implemented

--- app/javascript/modules/navbar.js
+++ app/javascript/modules/navbar.js
@@ -9,3 +9,26 @@ window.onscroll = function () {
     }
     prevScrollpos = currentScrollPos;
 }
+
+// calls menu on hamburger icon click
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
