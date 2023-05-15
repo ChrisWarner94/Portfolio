ecdd75510fb8851793ab6ea1d75e3aefc2e87c5a
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-05-15 11:11:40 +0100

    navbar container now introduced to keep navbar at top of page regardless of current page section

--- app/javascript/modules/home.js
+++ app/javascript/modules/home.js
@@ -2,12 +2,6 @@ const headingOne = "Christopher Warner"
 const headingTwo = "Full-Stack Developer"
 
 
-window.addEventListener('popstate', function (event) {
-    if (event.state) {
-        placeCaret();
-        this.alert("hello")
-    }
-}, false);
 
 function placeCaret() {
     var targetthree = document.getElementById("namecursor")
@@ -21,24 +15,12 @@ placeCaret();
 async function runTypeEffect() {
     await typeEffect(nameheading, headingOne);
     await typeEffect(jobtitleheading, headingTwo);
-    await fadeInRighta(navbar, 3);
-    await fadeInRighta(linkone, .0000000001,);
-    await fadeInRighta(linktwo, .0000000001,);
-    await fadeInRighta(linkthree, .0000000001);
-    await fadeInRighta(linkfour, .0000000001);
-    await fadeInRighta(linkfive, .0000000001);
-    await fadeInRighta(homepagelinkscontainer, 3);
-    await fadeInRighta(buttonblue, 3);
-    setAsVisible();
 }
 
 
 
 
-function setAsVisible() {
-    sectioncontact.style.display = "block"
-    sectionproject.style.display = "block"
-}
+
 
 function typeEffect(element, text) {
     return new Promise((resolve) => {
@@ -68,88 +50,3 @@ function typeEffect(element, text) {
 
 
 
-function fadeInRighta(element, speed) {
-    return new Promise((resolve) => {
-        let opacity = 0;
-        let bottom = 20;
-        element.style.display = "flex"
-        element.style.opacity = opacity;
-        element.style.position = 'relative';
-        element.style.bottom = bottom + 'px';
-        const intervalId = setInterval(() => {
-            opacity += 0.05;
-            bottom -= 1;
-            element.style.opacity = opacity;
-            element.style.bottom = bottom + 'px';
-            if (opacity >= 1) {
-                clearInterval(intervalId);
-                resolve();
-            }
-        }, speed);
-    });
-
-}
-
-function fadeInRight(element, speed) {
-    return new Promise((resolve) => {
-        let opacity = 0;
-        let left = 20;
-        element.style.display = "grid"
-        element.style.opacity = opacity;
-        element.style.position = 'relative';
-        element.style.left = left + 'px';
-        const intervalId = setInterval(() => {
-            opacity += 0.05;
-            left -= 1;
-            element.style.opacity = opacity;
-            element.style.left = left + 'px';
-            if (opacity >= 1) {
-                clearInterval(intervalId);
-                resolve();
-            }
-        }, speed);
-    });
-
-}
-
-function fadeInBottom(element, speed) {
-    element.innerHTML = "Hello there! I'm thrilled to welcome you to my profile site. I'm a Full Stack Web Developer with a passion for creating visually appealing and user-friendly web applications that provide meaningful experiences to users. With a keen eye for design and a knack for problem-solving, I enjoy bringing ideas to life using the latest web technologies and techniques. Take a moment to explore my profile site and learn more about my skills."
-    return new Promise((resolve) => {
-        let opacity = 0;
-        let top = 20;
-        element.style.opacity = opacity;
-        element.style.position = 'relative';
-        element.style.top = top + 'px';
-        const intervalId = setInterval(() => {
-            opacity += 0.05;
-            top -= 1;
-            element.style.opacity = opacity;
-            element.style.top = top + 'px';
-            if (opacity >= 1) {
-                clearInterval(intervalId);
-                resolve();
-            }
-        }, speed);
-    });
-}
-
-function fadeInBottomb(element, speed) {
-    return new Promise((resolve) => {
-        let opacity = 0;
-        let left = 20;
-        element.style.display = "flex"
-        element.style.opacity = opacity;
-        element.style.position = 'relative';
-        element.style.left = left + 'px';
-        const intervalId = setInterval(() => {
-            opacity += 0.05;
-            left -= 1;
-            element.style.opacity = opacity;
-            element.style.left = left + 'px';
-            if (opacity >= 1) {
-                clearInterval(intervalId);
-                resolve();
-            }
-        }, speed);
-    });
-}
