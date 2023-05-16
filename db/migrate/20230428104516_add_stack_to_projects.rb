de28a665cbbbdd6379b0671e55b4c891f25429c9
ChrisWarner94 <c.warner1994@hotmail.co.uk>
2023-05-16 01:55:33 +0100

    heroku install

--- db/migrate/20230428104516_add_stack_to_projects.rb
+++ db/migrate/20230428104516_add_stack_to_projects.rb
@@ -1,5 +1,6 @@
 class AddStackToProjects < ActiveRecord::Migration[7.0]
   def change
-    add_column :projects, :stack, :string, array: true
+    
   end
+  add_column :projects, :stack, :string, array: true
 end
