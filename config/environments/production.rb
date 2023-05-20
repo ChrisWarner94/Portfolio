559a56bdd819950536f57f6b3566f2e2de740e47
Christopher Warner <c.warner1994@hotmail.co.uk>
2023-05-20 13:50:15 +0100

    config host returned to original

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'www.christopher-warner.co.uk' }
+  config.action_mailer.default_url_options = { host: 'https://christopher-warner.co.uk' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
